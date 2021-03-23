c16f5c095252056dc1256e0e9ce79856a99cc757
YoonSoo <fhkaldh@naver.com>
2021-03-24 02:00:04 +0900

    21.03.24

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 3000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        { isLoading ?
+          (
+            "Loading..."
+          ) : (
+            "Ready!"
+          )
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
