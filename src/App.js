ba03a12b2541e20bed9d3a564e2f11af50542c9c
YoonSoo <fhkaldh@naver.com>
2021-03-25 21:35:37 +0900

    21.03.25

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -6,26 +9,45 @@ class App extends React.Component {
     movies: []
   };
 
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating');
+    this.setState({isLoading: false, movies});
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        { isLoading ?
+      <section className='container'>
+        {isLoading ? 
           (
-            "Loading..."
+            <div className='loader'>
+              <span className='loader__text'>Loading...</span>
+            </div>
           ) : (
-            "Ready!"
+            <div className='movies'>
+              {
+                movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+                ))
+              }
+            </div>
           )
         }
-      </div>
-    )
-  }
+      </section>
+    );
+  };
 }
 
 export default App;
